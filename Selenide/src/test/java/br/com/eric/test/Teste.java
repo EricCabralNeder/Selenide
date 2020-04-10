c1ecd42420c5cb927f5c967da9b7283847f2fe88
Eric Neder <ericbiu@hotmail.com>
2020-04-10 13:59:29 -0300

    Teste de Commit

--- Selenide/src/test/java/br/com/eric/test/Teste.java
+++ Selenide/src/test/java/br/com/eric/test/Teste.java
@@ -19,53 +19,10 @@ public class Teste extends BaseTestCase{
 	public void assertHomeScreenTitle() {
 
 		TestePage.clickHomeMenuItem();
+		System.out.println("Teste Reprovado");
 	}
 
-	
-//	/**
-//	 * Objetivo: Navegar para a pagina Curso Selenium e verificar o titulo exibido
-//	 * 
-//	 * Passos: Navegar para a pagina Curso Selenium, em seguida verificar o titulo exibido
-//	 * 
-//	 * Resultado Esperado:Titulo da p�gina est� correto
-//	 */
-//	@Test
-//	public void navigateToCursoSeleniumPage() {
-////		HomePage.clickSeleniumMenuItem();
-////		HomePage.clicksubMenuItem("Curso Selenium");
-////		HomePage.isHeaderTitleCorrect("Curso Selenium");
-//	}
-//	
-//	
-//	/**
-//	 * Objetivo: Navegar para a pagina Formulario Simples e verificar o titulo
-//	 * 
-//	 * Passos: Navegar para a pagina Formulario Simples e verificar o titulo exibido
-//	 * 
-//	 * Resultado Esperado:Titulo da p�gina est� correto
-//	 */
-//	@Test
-//	public void navigateToFormularioSimplesPage() {
-////		HomePage.clickSeleniumMenuItem();
-////		HomePage.clicksubMenuItem("Formulário Simples");
-////		HomePage.isHeaderTitleCorrect("Formulário Simples");
-//	}
-//	
-//	
-//	/**
-//	 * Objetivo: P�gina de resultados da busca � exibida corretamente
-//	 * 
-//	 * Passos: Realiza busca com texto fixo e verifica o t�tulo da p�gina de resultados
-//	 * 
-//	 * Resultado Esperado: T�tulo da p�gina de resultados est� correto
-//	 */
-//	@Test
-//	public void performSimpleSearch() {
-////		HomePage.showHiddenOptions();
-////		HomePage.performSearch("teste");
-////		HomePage.clickSearchButton();
-////		HomePage.isSearchResultCorrect("Resultados da pesquisa por: teste");
-//	}
+
 
 	
 	@After
