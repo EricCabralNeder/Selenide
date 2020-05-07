cf1f5ecdcb2a75ef1b0ecb8fb495944267979c55
Eric Neder <ericbiu@hotmail.com>
2020-05-07 14:52:36 -0300

    Iniciando testes

--- Selenide/src/test/java/br/com/eric/test/Login.java
+++ Selenide/src/test/java/br/com/eric/test/Login.java
@@ -1,14 +1,29 @@
 package br.com.eric.test;
 
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByClassName;
+import org.openqa.selenium.WebElement;
+
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.Selectors.ByText;
+import com.codeborne.selenide.commands.SetValue;
+
+import static com.codeborne.selenide.WebDriverRunner.isChrome;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Condition.text;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import br.com.eric.common.TestePage;
 
 
 
-public class Login extends BaseTestCase{
+
+public class Login{
 	
 	@Before
 	public void before() throws Exception {  
@@ -16,10 +31,20 @@ public class Login extends BaseTestCase{
 
 	
 	@Test
-	public void assertHomeScreenTitle() {
-
-		TestePage.clickLogin();
-		System.out.println("Teste Reprovado");
+	public void usuarioLogado() {
+		isChrome();
+		open("http://ninjaplus-web:5000/login");
+		
+		//$("#emailId").setValue("ericbiu@hotmail.com"); busca por id
+		$("input[name=email]").setValue("ericbiu@hotmail.com"); //busca por input name
+		$("#passId").setValue("99918278");
+		$(byText("Entrar")).click();
+		//$("#login").click();
+		
+		System.out.println(".." + $(".user .info .span").getText() + "..");
+		
+		$(".user .info .span").shouldHave(text("eric"));   //verifica se a condição foi ayendida !!
+	
 	}
 
 
